b74dc278afb0d5b04bbb6a9e4a1573081aa4d086
TDrake38 <thomasdrake_38@hotmail.com>
2022-02-24 23:29:20 -0330

    added some css for the player circles and started on the js

---
+++ app.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const squares = document.querySelectorAll('.grid')
+    const result = document.querySelector('#result')
+    const displayCurrentPlayer = document.querySelector('#current-player')
+})
