53437855298d3d968d3cb660f72a06c5fc339c04
TDrake38 <thomasdrake_38@hotmail.com>
2022-02-25 20:35:04 -0330

    added some js to click and add circles to the connect four

--- app.js
+++ app.js
@@ -2,4 +2,31 @@ document.addEventListener('DOMContentLoaded', () => {
     const squares = document.querySelectorAll('.grid')
     const result = document.querySelector('#result')
     const displayCurrentPlayer = document.querySelector('#current-player')
+    let currentPlayer = 1
+
+    for (let i = 0, len = squares.length; i < len; i++)
+
+        (function(index){
+        //add an onlcick to each square in the grid
+            squares[i].onClick = function(){
+            // if the square below your current square is taken, you can go ontop of it
+                if(squares[index + 7].classList.contains('taken')) {
+                    if (currentPlayer === 1) {
+                        squares[index].classList.add('taken')
+                        squares[index].classList.add('player-one')
+                        // change the player
+                        currentPlayer = 2
+                        displayCurrentPlayer.innerHTML = currentPlayer
+                    } else if (currentPlayer === 2) {
+                        squares[index].classList.add('taken')
+                        squares[index].classList.add('player-two')
+                        // change the player
+                        currentPlayer = 1
+                        displayCurrentPlayer.innerHTML = currentPlayer
+                    }
+                    // if the square below your current square is not taken, you cant go here
+                } else alert('Can not go here')
+            }
+
+        })
 })
